4ea7ae5c2106df18ce93353143c6873ca5b4a441
Layla Rodas <rodas.layla@gmail.com>
2024-08-29 14:43:14 +0200

    config view

--- server/index.js
+++ server/index.js
@@ -4,8 +4,15 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 
+app.use(express.static('client'))
+
+
 app.get('/', function (req, res) {
     res.status(200).send('test route')
+});
+
+io.on('connection', function (socket) {
+    console.log('The customer with IP: ' + socket.handshake.address + 'has connected')
 })
 
 
